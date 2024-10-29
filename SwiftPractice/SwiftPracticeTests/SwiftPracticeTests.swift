b0de7dcd6f74a1ab17fe537625717a2aecbf9e66
juancalderon <juanestebancalderonovalle@gmail.com>
2024-10-29 18:28:46 -0500

    practice scroll view

---
+++ SwiftPractice/SwiftPracticeTests/SwiftPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPracticeTests.swift
+//  SwiftPracticeTests
+//
+//  Created by Juan Esteban Calderon Ovalle on 28/10/24.
+//
+
+import Testing
+@testable import SwiftPractice
+
+struct SwiftPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
